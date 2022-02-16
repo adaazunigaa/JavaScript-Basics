0a5224b233f56c45bd768c4dbf479b5a4e22d1a8
AdaZuniga <56266464+AdaZuniga@users.noreply.github.com>
2022-02-16 16:33:09 -0600

    JS Nesting arrays and objects
    Syntax

--- basics.js
+++ basics.js
@@ -1,1 +1,7 @@
 console.log("Hey hey hey \n hey")
+
+// NESTING ARRAYS AND OBJECTS
+    const comments = [
+        {username: "Tammy", text: "lolololo", votes: 9},
+        {username: "FishB", text: "glub glub", votes: 123458},
+    ]
