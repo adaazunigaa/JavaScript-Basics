7a609327ae76677d0f99f214ad9091123e86a54c
AdaZuniga <56266464+AdaZuniga@users.noreply.github.com>
2022-02-16 21:11:38 -0600

    Iterating Over Objects
    -Utilize the 'for ... in ... ' loop
    -Object method does not have .length

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 JS BASIC CONCEPTS 
 -loops
     -for ... of ...
+    -for ... in ... 
+    -Object method to iterate an object 
+    
--- basics.js
+++ basics.js
@@ -27,3 +27,46 @@ for (let char of "hello world")
 {
     console.log(char.toUpperCase());
 }
+
+
+// for ... in ... loop 
+// used when wanting to iterat wiht objects 
+const testScores = {
+    Josh    : 85,
+    Lam     : 75,
+    Murray  : 94,   
+    Anil    : 86
+}
+
+//this will give us the key 
+for ( let person in testScores){
+    console.log(person);
+}
+
+for ( let person in testScores){
+    console.log(`${person} scored ${testScores[person]}`);
+}
+
+
+//method Object to iterate an object 
+console.log(Object.keys(testScores));
+console.log(Object.values(testScores));
+console.log(Object.entries(testScores));
+
+// let totalScore = 0;
+// for (let score of Object.values(testScores)){
+//     totalScore+= score;
+// }
+
+// console.log("Total score is: " + totalScore);
+
+let totalScore = 0;
+let scores = Object.values(testScores);
+for (let score of scores){
+    totalScore+= score;
+}
+
+console.log("The average score is: " + (totalScore / scores.length));
+
+
+
