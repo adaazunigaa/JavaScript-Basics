b1f411788533fc783b6e92c8e01ad93f4e071b9c
AdaZuniga <56266464+AdaZuniga@users.noreply.github.com>
2022-02-20 13:34:05 -0600

    returning functions
    A fucntion that returns a function, that has no name. Functions are values in js)

--- Leveling up Functions/functions.js
+++ Leveling up Functions/functions.js
@@ -1,8 +1,8 @@
 // LEXICAL SCOPE *****************************
-function bankRobbery(){
+function bankRobbery() {
     const heros = ["Spiderman", "Wolverine", "Iron man"];
-    function help(){
-        for (let hero of heros){
+    function help() {
+        for (let hero of heros) {
             console.log(`please help me ${hero.toLocaleUpperCase()}`);
         }
     }
@@ -14,25 +14,65 @@ function bankRobbery(){
 // FUNCTION EXPRESSIONS ***************************
 //  different syntax for defining a function
 //  functions are values 
-const add = function(x,y){
+const add = function (x, y) {
     return x + y;
 }
 
-const square = function (num){
-    return (num*num);
+const square = function (num) {
+    return (num * num);
 }
 
 
 
 // HIGHER ORDER FUNCTIONS *****************************
-function callTwice(func){
+function callTwice(func) {
     func();
     func();
 }
 
-function rollDie(){
+function rollDie() {
     const roll = Math.floor(Math.random() * 6) + 1;
     console.log(roll);
 }
 
 callTwice(rollDie);
+
+
+
+
+
+
+
+//  RETURNING FUNCTIONS ***********************************
+function makeMysteryFunc() {
+    const rand = Math.random();
+    if (rand > .5) {
+        return function () {
+            console.log("Congrats! I am a GOOD Function")
+        }
+    }
+    else {
+        return function () {
+            alert("NEW VIRUS");
+            alert("STOP CLOSING THE WINDOW!");
+        }
+    }
+}
+//on the console
+//let x = makeMysteryFunc();
+//x();
+
+
+
+
+function makeBetweenFunc(min, max) {
+    return function (num) {
+        return num >= min && num <= max;
+    }
+}
+
+let child = makeBetweenFunc(0, 18);
+console.log(child(5)); //it will return true becasue 5 is between 0 and 18
+
+let adult = makeBetweenFunc(19, 65);
+console.log(adult(18)); //will return false because 18 is led 
